12ea1b8ac5be29c23518fd610e218cba3ecadcfb
mrghosti3 <mysteriousghst@gmail.com>
2020-02-26 19:30:09 +0200

    Added unit tests

---
+++ GildedRoseKata.Tests/GildedRoseTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GildedRoseKata.Tests
+{
+    [TestClass]
+    public class GildedRoseTests
+    {
+    }
+}
---
+++ GildedRoseKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GildedRoseKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GildedRoseKata.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("01f0460c-bfe3-4feb-97c4-b408dcdb01ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
