4788ec16b9ac238e58a36425d46ef2e616ec4bca
mrghosti3 <mysteriousghst@gmail.com>
2020-02-27 11:13:48 +0200

    Added 2 test methods

--- GildedRoseKata.Tests/GildedRoseTests.cs
+++ GildedRoseKata.Tests/GildedRoseTests.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace GildedRoseKata.Tests
@@ -6,5 +6,39 @@ namespace GildedRoseKata.Tests
     [TestClass]
     public class GildedRoseTests
     {
+        [TestMethod]
+        public void UpdateQuality_AgedBrie_UpdateCorrect()
+        {
+            // Init
+            IList<Item> agedBrie = new List<Item>()
+            {
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
+            };
+            GildedRose app = new GildedRose(agedBrie);
+
+            //Action
+            app.UpdateQuality();
+
+            //Assert
+            Assert.IsTrue(agedBrie[0].Quality == 1);
+        }
+
+        [TestMethod]
+        public void UpdateQuality_AgedBrieX2_UpdateCorect()
+        {
+            // Init
+            IList<Item> agedBrie = new List<Item>()
+            {
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 }
+            };
+            GildedRose app = new GildedRose(agedBrie);
+
+            //Action
+            app.UpdateQuality();
+            app.UpdateQuality();
+
+            //Assert
+            Assert.IsTrue(agedBrie[0].Quality == 2);
+        }
     }
 }
