5264867427de681a41ae7a7057c7e69241eadacf
mrghosti3 <mysteriousghst@gmail.com>
2020-02-27 12:09:02 +0200

    Added tests for "Backstage passes" calculation

--- GildedRoseKata.Tests/GildedRoseTests.cs
+++ GildedRoseKata.Tests/GildedRoseTests.cs
@@ -40,5 +40,93 @@ namespace GildedRoseKata.Tests
             //Assert
             Assert.IsTrue(agedBrie[0].Quality == 2);
         }
+
+        [TestMethod]
+        public void UpdateQuality_BackstagePasses_UpdateCorrect()
+        {
+            //Arrange
+            IList<Item> backstagePass = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(backstagePass);
+
+            //Action
+            app.UpdateQuality(2);
+
+            //Assert
+            Assert.IsTrue(backstagePass[0].Quality == 21);
+        }
+
+        [TestMethod]
+        public void UpdateQuality_BackstagePasses2_UpdateCorrect()
+        {
+            //Arrange
+            IList<Item> backstagePass = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(backstagePass);
+
+            //Action
+            app.UpdateQuality(5);
+
+            //Assert
+            Assert.IsTrue(backstagePass[0].Quality == 22);
+        }
+
+        [TestMethod]
+        public void UpdateQuality_BackstagePasses3_UpdateCorrect()
+        {
+            //Arrange
+            IList<Item> backstagePass = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(backstagePass);
+
+            //Action
+            app.UpdateQuality(10);
+
+            //Assert
+            Assert.IsTrue(backstagePass[0].Quality == 23);
+        }
+
+        [TestMethod]
+        public void UpdateQuality_BackstagePasses4_UpdateCorrect()
+        {
+            //Arrange
+            IList<Item> backstagePass = new List<Item>()
+            {
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(backstagePass);
+
+            //Action
+            app.UpdateQuality(16);
+
+            //Assert
+            Assert.IsTrue(backstagePass[0].Quality == 0);
+        }
     }
 }
